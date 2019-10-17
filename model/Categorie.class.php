21d33115aa4554cd27d9c499fbe1fbd22710a0fb
VIELLARD Bruno <viellarb@hostname.iut2.upmf-grenoble.fr>
2019-10-17 10:47:54 +0200

    Classe Caracteristique.class.php

--- model/Categorie.class.php
+++ model/Categorie.class.php
@@ -2,10 +2,10 @@
 
   // Classe pour les catégories d'articles vendus sur le site
   class Categorie {
-      private $id;                // Identifiant de la catégorie
-      private $intitule;          // Nom de la catégorie
-      private $categorieMere;     // Catégorie mère de la catégorie (elle même si elle n'en possède pas)
-      private $taxe               // Taux de TVA de la catégorie
+      private $id;            // Identifiant de la catégorie
+      private $intitule;      // Nom de la catégorie
+      private $categorieMere; // Catégorie mère de la catégorie (elle même si elle n'en possède pas)
+      private $taxe           // Taux de TVA de la catégorie
 
       //Getteurs
       function getId() : int {
